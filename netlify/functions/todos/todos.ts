43ee1e34cc8909189f099fa604e36134020d2694
Moonpearl <romain.tello@gmail.com>
2021-11-15 15:48:53 +0100

    Add instructions

--- netlify/functions/todos/todos.ts
+++ netlify/functions/todos/todos.ts
@@ -13,7 +13,18 @@ const {
   DB_NAME,
 } = process.env;
 
+// GET /todos        => Récupère toutes les tâches à faire
+// GET /todos/:id    => Récupère une tâche à faire en particulier
+// POST /todos       => Créer une tâche à faire
+// PATCH /todos/:id  => Modifier une tâche à faire existante en particulier
+// PUT /todos/:id    => Remplacer une tâche à faire existante en particulier
+// DELETE /todos/:id => Supprimer une tâche à faire existante en particulier
+
 export const handler: Handler = async (event, context) => {
+  // event.body   // Récupère le corps de la requête
+  // event.httpMethod   // Récupère la méthode HTTP de la requête
+  // event.path   // Récupère l'URL demandée par l'utilisateur
+
   // Establish connection with database
   const db = createConnectionPool(
     `postgres://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
